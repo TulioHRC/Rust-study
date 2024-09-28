caa4a8bedffb4da612adbd9ff9276a836c7fb95d
TulioHRC <grove1411@gmail.com>
2024-09-28 12:34:37 -0300

    test: parse_config tests

--- Tutorials/017 - Command Line Project/grep_clone/src/lib.rs
+++ Tutorials/017 - Command Line Project/grep_clone/src/lib.rs
@@ -19,3 +19,26 @@ pub fn run(command: &String, file_path: &String) -> Result<(), Box<dyn error::Er
 
   return Ok(());
 }
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn parse_config_less_arguments(){
+    let args: Vec<String> = vec!["Test".to_string()];
+    assert_eq!(parse_config(&args), Err("Not enough arguments!"));
+  }
+
+  #[test]
+  fn parse_config_right_arguments(){
+    let args: Vec<String> = vec![String::from("binary_path"), String::from("test"), String::from("test.txt")];
+    assert_eq!(parse_config(&args), Ok((&"test".to_string(), &"test.txt".to_string())));
+  }
+
+  #[test]
+  fn parse_config_more_arguments(){
+    let args: Vec<String> = vec![String::from("binary_path"), String::from("test"), String::from("test.txt"), String::from("additional_argument")];
+    assert_eq!(parse_config(&args), Ok((&"test".to_string(), &"test.txt".to_string())));
+  }
+}
