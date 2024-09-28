35cd3a050f4a49e5dd6d791fbaf290e23df98846
TulioHRC <grove1411@gmail.com>
2024-09-28 11:53:53 -0300

    feat: add read_file

--- Tutorials/017 - Command Line Project/grep_clone/src/main.rs
+++ Tutorials/017 - Command Line Project/grep_clone/src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let command = &args[1];
+    let file_path = &args[2];
+
+    dbg!(&args);
+
+    println!("Reading file {file_path}:");
+
+    let file_content = fs::read_to_string(file_path)
+        .expect("Couldn't read the file.");
+
+    println!("\n{file_content}");
 }
